5a7d96389a8c68ae1d60c7cf6e71aa29c6205710
madiscercy <madiscercy@gmail.com>
2023-05-01 20:28:48 -0400

    starting to make paths

--- models/Category.js
+++ models/Category.js
@@ -5,26 +5,25 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
 
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
+		category_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'category',
+	}
 );
 
 module.exports = Category;
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,35 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+	// find all categories
+	// be sure to include its associated Products
+	try {
+		const categoryData = await Category.findAll({
+			include: [{ model: Product }],
+		});
+		console.log(categoryData);
+		res.status(200).json(categoryData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+	// find one category by its `id` value
+	// be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+	// create a new category
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+	// update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+	// delete a category by its `id` value
 });
 
 module.exports = router;
